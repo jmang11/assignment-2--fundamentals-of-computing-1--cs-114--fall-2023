e201d438baf63c0f1286673aba5a50c17d86c8ac
jmang11 <mangiamel@hartford.edu>
2023-10-11 09:49:00 -0400

    code

--- Diamond.java
+++ Diamond.java
@@ -8,28 +8,47 @@ public class Diamond {
 
     System.out.println("Enter an integer:");
     i = scan.nextInt();
-
-    //if(i % 2 == 0){
-      int count = 1;
-      //System.out.println("even");
-      for(int a = 1; a <= 2 * i - 1; a++){
-        for(int b = count; b <= i; b++){
+    int count = 0;
+    if(i % 2 == 0){     //Identify even input
+      for(int a = count; a <= i; a++){
+        for(int b = count; b <= (i -(i/2)); b++){
           System.out.print("  ");
         }
-        for(int c = 1; c <= count * 2 - 1; c++){
+        for(int c = 1; c <= count * 2; c++){
           System.out.print("* ");
         }
-        if(a < i){
+        for(int d = count; d < i - (i-1); d++){
+          System.out.print("* ");
+        }
+        if(a < (i -(i/2))){
           count++;
         }
         else{
           count--;
         }
         System.out.println("");
-      }
+
+
+
     }
-    //else{
-      //System.out.println("odd");
+
     }
-  //}
-//}
+    else{     //Identify odd input
+      for(int a = 1; a <= i; a++){
+        for(int b = count; b <= i - 1; b++){
+          System.out.print("  ");
+        }
+        for(int c = 1; c <= count * 2 - 1; c++){
+          System.out.print("* ");
+        }
+        if(a < (i -(i/2))){
+          count++;
+        }
+        else{
+          count--;
+        }
+        System.out.println("");
+    }
+  }
+}
+}
